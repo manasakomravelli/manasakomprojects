37121895918c7a03fd00a53b5aa638830f5bd3b7
Manasa Komuravelli <manchikomuravelli@Manasas-MBP.attlocal.net>
2023-05-23 23:26:00 -0700

    initialcommit

---
+++ src/first/MyFirst.java
@@ -0,0 +1,25 @@
+package first;
+
+public class MyFirst {
+	
+	
+	public void manasasTalent() {
+		System.out.println("Its my talent");
+	}
+
+	public static void main(String[] args) {
+		
+		System.out.print("test \n");
+        System.out.println("test2");
+        System.out.print("test \n");
+        System.out.println("test2");
+        System.out.print("test \n");
+        System.out.println("test2");
+        System.out.print("test \n");
+        System.out.println("test2");
+        System.out.print("test \n");
+        System.out.println("test2");
+        System.out.println(MyFirst.class);
+	}
+
+}
